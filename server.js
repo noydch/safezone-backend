0f27dbd8a5afc508ec1787a716a257cefd21f563
noydch <noydch2003@gmail.com>
2025-09-16 11:54:42 +0700

    addHost

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors({
         'http://localhost:5173',
         'http://localhost:5174',
         'https://mysafezone.netlify.app',
-        'https://mysafezone-mb.netlify.app'
+        'https://saysamone1.wasmer.app/'
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
